c4db8e2ed5e0f450870de5e0aa43c68907ca0ca9
zhenglicheng <zhenglicheng@corp.netease.com>
2025-09-13 11:19:00 +0800

    package all in same spm

--- Package.swift
+++ Package.swift
@@ -4,36 +4,44 @@
 import PackageDescription
 
 let package = Package(
-  name: "SwiftyLua",
-  platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13),],
-  products: [
+    name: "SwiftyLua",
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13),],
+    products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
-      name: "SwiftyLua",
-      targets: ["SwiftyLua"]),
-  ],
-  dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
-    .package(url: "https://github.com/SwiftyLua/lua4swift", from: "1.1.3"),
-    .package(url: "https://github.com/Quick/Quick", from: "7.6.2"),
-    .package(url: "https://github.com/Quick/Nimble", from: "13.7.1")
-  ],
-  targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages this package depends on.
-    .target(
-      name: "SwiftyLua",
-      dependencies: [
-        "lua4swift"
-      ]),
-    .testTarget(
-      name: "SwiftyLuaTests",
-      dependencies: [
-        "SwiftyLua",
-        "Quick",
-        "Nimble"
-      ],
-      resources: [.copy("LuaScripts")]),
-  ]
+        name: "SwiftyLua",
+        targets: ["SwiftyLua"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Quick/Quick", from: "7.6.2"),
+        .package(url: "https://github.com/Quick/Nimble", from: "13.7.1")
+    ],
+    targets: [
+        .target(
+            name: "SwiftyLua",
+            dependencies: ["lua4swift"]),
+        .target(
+            name: "CLua",
+            path: "Sources/CLua",
+            publicHeadersPath: "include"
+        ),
+        .target(
+            name: "lua4swift",
+            dependencies: ["CLua"],
+            path: "Sources/lua4swift",
+            linkerSettings: [.linkedLibrary("z"), .linkedFramework("Foundation")]
+         ),
+        .testTarget(
+            name: "SwiftyLuaTests",
+            dependencies: [
+                .target(name: "SwiftyLua"),
+                .product(name: "Quick", package: "Quick"),
+                .product(name: "Nimble", package: "Nimble")
+            ],
+            resources: [.copy("LuaScripts")]),
+    ],
+    cLanguageStandard: .gnu99,
+    cxxLanguageStandard: .cxx11
 )
